47735127cda65fbcce76022a848cffed7d56e492
Your Name <you@example.com>
2016-07-25 00:42:44 +0000

    made locations add-delete features

--- gmaps/urls.py
+++ gmaps/urls.py
@@ -6,4 +6,6 @@ app_name = "gmaps"
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^add/(?P<location>[a-zA-Z0-9]+)/$', views.add_location, name='add_location'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.delete_location, name='delete_location'),
 ]
--- gmaps/views.py
+++ gmaps/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+
+from django.utils import timezone
 
 from .models import Location
 
@@ -17,6 +23,16 @@ class DetailView(generic.DetailView):
     model = Location
     template_name = 'gmaps/detail.html'
 
+def add_location(request, location):
+    l = Location(location=location, pub_date=timezone.now())
+    l.save()
+    return HttpResponseRedirect(reverse('gmaps:index'))
+    #return HttpResponse("You're looking at location %s." % location)
+
+def delete_location(request, pk):
+    location = get_object_or_404(Location, pk=pk)
+    location.delete()
+    return HttpResponse("You're looking at deleting location %s." % location)
 
 """
 class ResultsView(generic.DetailView):
